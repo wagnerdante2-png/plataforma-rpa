3e9f384196aa86df777ab6547e3862642a38cfeb
D4N73 <wagnerdante2@gmail.com>
2026-09-21 09:22:55 -0300

    Reduz velocidade do efeito Matrix

--- web/app.js
+++ web/app.js
@@ -166,7 +166,7 @@ function drawMatrix() {
     const y = drops[i] * fontSize;
     ctx.fillText(char, i * fontSize, y);
     if (y > innerHeight && Math.random() > .975) drops[i] = Math.random() * -20;
-    drops[i] += .72 + Math.random() * .22;
+    drops[i] += .34 + Math.random() * .10;
   }
 
   requestAnimationFrame(drawMatrix);
